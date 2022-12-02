16d59739a76e91db7127e8097df300e43efc79a1
Serdar Buhan <serdar.buhan@toasttab.com>
2022-12-02 17:08:15 +0000

    Give RemoteFeedLoader a url instead of hardcoding it.

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -8,23 +8,18 @@
 import XCTest
 
 class RemoteFeedLoader {
+    let url: URL    // user of RemoteFeedLoader needs to know this url. Not the remote feed loader. It should be able to load from any url.
     let client: HTTPClient
 
-    init(client: HTTPClient) { // RemoteFeedLoader does not locate the client anymore. Initializer/constructor injection
+    init(url: URL, client: HTTPClient) { // generic. production code is clean
+        self.url = url
         self.client = client
     }
 
     func load() {
-        client.get(from: URL(string: "http://a-url.com")!) // No need to locate the client. It's passed in the initializer
+        client.get(from: url)
     }
 }
-
-// Shared instance is not needed anymore.
-// And there is no implementation of the load function. looks like an abstract class
-
-// class HTTPClient { // no need the shared instance anymore
-//    func get(from url: URL) {}
-//}
 
 protocol HTTPClient {
     func get(from url: URL)
@@ -42,21 +37,20 @@ class HTTPClientSpy: HTTPClient {
 final class RemoteFeedLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
+        let url = URL(string: "http://a-url.com")!
         let client = HTTPClientSpy()
-        _ = RemoteFeedLoader(client: client)
+        _ = RemoteFeedLoader(url: url, client: client)
 
         XCTAssertNil(client.requestedURL)
     }
 
     func test_load_requestDataFromURL() {
-        // Arrange
+        let url = URL(string: "https://a-given-url.com")! // test case urls and logic is completely on the test side
         let client = HTTPClientSpy()
-        let sut = RemoteFeedLoader(client: client)
+        let sut = RemoteFeedLoader(url: url, client: client)
 
-        // Act
         sut.load()
 
-        // Assert
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, url)
     }
 }
