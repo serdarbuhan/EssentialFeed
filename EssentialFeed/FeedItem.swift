d38dac1756df68fa9fd5606b2485265d4dc777a7
Serdar Buhan <serdar.buhan@toasttab.com>
2022-12-02 15:53:58 +0000

    Create the FeedItem model

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Serdar Buhan on 02/12/2022.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
