37333439679c434128e326cb26fbd03ab6f6ea96
Serdar Buhan <serdar.buhan@toasttab.com>
2022-12-03 14:38:15 +0000

    Delivers items array on 200 HTTP response with JSON items

--- EssentialFeed/FeedAPI/RemoteFeedLoader.swift
+++ EssentialFeed/FeedAPI/RemoteFeedLoader.swift
@@ -41,9 +41,8 @@ public final class RemoteFeedLoader {
             switch result {
             case let .success(data, _):
 
-                // check if json is valid return empty for now
-                if let _ = try? JSONSerialization.jsonObject(with: data) {
-                    completion(.success([]))
+                if let root = try? JSONDecoder().decode(Root.self, from: data) {
+                    completion(.success(root.items))
                 } else {
                     completion(.failure(.invalidData))
                 }
@@ -52,4 +51,9 @@ public final class RemoteFeedLoader {
             }
         }
     }
+}
+
+// Decode the root object from the json
+private struct Root: Decodable {
+    let items: [FeedItem]
 }
--- EssentialFeed/FeedFeature/FeedItem.swift
+++ EssentialFeed/FeedFeature/FeedItem.swift
@@ -10,8 +10,26 @@ import Foundation
 // made it public so RemoteFeedLoader can access
 // made it equatable for assertion in tests
 public struct FeedItem: Equatable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let imageURL: URL
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
+
+// Make FeedItem Decodable for JSON Decoder
+// But API details are leaking into FeedFeature module
+extension FeedItem: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case id
+        case description
+        case location
+        case imageURL = "image"
+    }
 }
