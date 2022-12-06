59522d22d308ce4bd28cedb14f62b637c5546c50
Serdar Buhan <buhanserdar@gmail.com>
2022-12-06 17:23:53 +0000

    Update travis yml and end to end test duration

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -46,7 +46,7 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
             receivedResult = result
             exp.fulfill()
         }
-        wait(for: [exp], timeout: 15.0)
+        wait(for: [exp], timeout: 25.0)
         return receivedResult
     }
 
