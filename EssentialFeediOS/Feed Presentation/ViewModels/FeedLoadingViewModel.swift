1caed1f64d74aef9192b79342459e79e259af98a
Serdar Buhan <buhanserdar@gmail.com>
2022-12-19 11:33:15 +0000

    Move ViewModels to separate files

---
+++ EssentialFeediOS/Feed Presentation/ViewModels/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Serdar Buhan on 19/12/2022.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeediOS/Feed Presentation/ViewModels/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Serdar Buhan on 19/12/2022.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
