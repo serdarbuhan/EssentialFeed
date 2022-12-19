1217c91a4de8959d97c92b3eae61a3aa5530189a
Serdar Buhan <buhanserdar@gmail.com>
2022-12-19 01:46:51 +0000

    Move `FeedImage` value translations (including image loading state) to `FeedImageViewModel`. Now, the `FeedImageCellController` acts as a binder between the `View` and the `ViewModel`

--- EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/Feed UI/Composers/FeedUIComposer.swift
@@ -22,7 +22,8 @@ public final class FeedUIComposer {
     private static func adaptFeedToCellControllers(forwardingTo controller: FeedViewController, loader: FeedImageDataLoader) -> ([FeedImage]) -> Void {
         return { [weak controller] feed in
             controller?.tableModel = feed.map { model in
-                FeedImageCellController(model: model, imageLoader: loader)
+                let feedImageViewModel = FeedImageViewModel(model: model, imageLoader: loader)
+                return FeedImageCellController(viewModel: feedImageViewModel)
             }
         }
     }
