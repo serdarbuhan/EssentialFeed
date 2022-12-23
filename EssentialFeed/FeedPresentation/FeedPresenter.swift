e25d36dda2fe01e5bb199a7b8e5477a32d1b9537
Serdar Buhan <buhanserdar@gmail.com>
2022-12-23 11:54:58 +0000

    Move `FeedViewModel` to separate file

--- EssentialFeed/FeedPresentation/FeedPresenter.swift
+++ EssentialFeed/FeedPresentation/FeedPresenter.swift
@@ -11,10 +11,6 @@ public struct FeedLoadingViewModel {
     public let isLoading: Bool
 }
 
-public struct FeedViewModel {
-    public let feed: [FeedImage]
-}
-
 public struct FeedErrorViewModel {
     public let message: String?
 
---
+++ EssentialFeed/FeedPresentation/FeedViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Serdar Buhan on 23/12/2022.
+//
+
+public struct FeedViewModel {
+    public let feed: [FeedImage]
+}
