f3667a4da602dbf717226db8084154bd0b8b1dd6
Serdar Buhan <buhanserdar@gmail.com>
2022-12-25 19:53:07 +0000

    Test FeedImageDataMapper in isolation

---
+++ EssentialFeed/FeedAPI/FeedImageDataMapper.swift
@@ -0,0 +1,22 @@
+//
+//  FeedImageDataMapper.swift
+//  EssentialFeed
+//
+//  Created by Serdar Buhan on 25/12/2022.
+//
+
+import Foundation
+
+public final class FeedImageDataMapper {
+    public enum Error: Swift.Error {
+        case invalidData
+    }
+
+    public static func map(_ data: Data, from response: HTTPURLResponse) throws -> Data {
+        guard response.isOK, !data.isEmpty else {
+            throw Error.invalidData
+        }
+
+        return data
+    }
+}
