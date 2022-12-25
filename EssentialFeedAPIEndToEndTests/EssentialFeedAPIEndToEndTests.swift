77348caf725708021a8c708174439e941eddce99
Serdar Buhan <buhanserdar@gmail.com>
2022-12-25 19:42:27 +0000

    Remove FeedLoader protocol as we don't need it anymore - we're composing the types with universal abstractions provided by the Combine framework
    FeedLoaderStub.swift and XCTestCase+LoaderSpy should have been removed earlier in the commit:
    `Replace FeedLoader composition with Combine operators - d60fc9a`

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -45,13 +45,13 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
 
     // MARK: - Helpers
 
-    private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> FeedLoader.Result? {
+    private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> Swift.Result<[FeedImage], Error>? {
 
         // Default cache folder /Users/serdarbuhan/Library/Caches/com.apple.dt.xctest.tool
         let client = ephemeralClient()
         let exp = expectation(description: "Wait for load completion")
 
-        var receivedResult: FeedLoader.Result?
+        var receivedResult: Swift.Result<[FeedImage], Error>?
         client.get(from: feedTestServerURL) { result in
             receivedResult = result.flatMap { (data, response) in
                 do {
