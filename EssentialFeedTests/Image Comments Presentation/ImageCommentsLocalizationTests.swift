12dff2d6f003609be79e645180bb4b67ec64226e
Serdar Buhan <buhanserdar@gmail.com>
2022-12-27 09:31:18 +0000

    Localize image comments title

---
+++ EssentialFeedTests/Image Comments Presentation/ImageCommentsLocalizationTests.swift
@@ -0,0 +1,18 @@
+//
+//  ImageCommentsLocalizationTests.swift
+//  EssentialFeedTests
+//
+//  Created by Serdar Buhan on 27/12/2022.
+//
+
+import XCTest
+import EssentialFeed
+
+final class ImageCommentsLocalizationTests: XCTestCase {
+
+    func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
+        let table = "ImageComments"
+        let bundle = Bundle(for: ImageCommentsPresenter.self)
+        assertLocalizedKeyAndValuesExist(in: bundle, table)
+    }
+}
